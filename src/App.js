c01ff675dfd3cbe272a04248a642da21f67b58a6
Meng <@mengyang41>
2020-08-18 15:17:46 -0400

    add front end

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ActivityComponent from './components/ActivityComponent';
+import AddActivityComponent from './components/AddActivityComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <AddActivityComponent />
+    </div>
+  );
+}
+
+export default App;
